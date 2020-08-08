1a5b664cede053b96c14a8d2c99fba5df92dd1d9
이충희 <choonghee.dev@gmail.com>
2020-08-08 14:27:45 +0900

    ADD: 스타벅스 음료 데이터를 통한 장고 모델링
    [+] 장고 프로젝트, 앱 생성
    [+] 카테고리, 사이즈, 알러지 정보, 영양 정보, 음료, 설명, 이미지 모델 생성

---
+++ drinks/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = "categories"
+
+class Size(models.Model):
+    name_kr     = models.CharField(max_length=32)
+    name_en     = models.CharField(max_length=32)
+    milliliter  = models.DecimalField(max_digits=4, decimal_places=3)
+    fluid_ounce = models.DecimalField(max_digits=4, decimal_places=3)
+
+    class Meta:
+        db_table = "sizes"
+
+class Allergen(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = "allergens"
+
+class Nutrition(models.Model):
+    calorie       = models.DecimalField(max_digits=5, decimal_places=3)
+    saturated_fat = models.DecimalField(max_digits=5, decimal_places=3)
+    protein       = models.DecimalField(max_digits=5, decimal_places=3)
+    sodium        = models.DecimalField(max_digits=5, decimal_places=3)
+    sugar         = models.DecimalField(max_digits=5, decimal_places=3)
+    caffeine      = models.DecimalField(max_digits=5, decimal_places=3)
+
+    class Meta:
+        db_table = "nutritions"
+
+class Drink(models.Model):
+    name_kr   = models.CharField(max_length=128)
+    name_en   = models.CharField(max_length=128)
+    category  = models.ForeignKey("Category", on_delete=models.CASCADE)
+    size      = models.ForeignKey("Size", on_delete=models.CASCADE)
+    nutrition = models.OneToOneField("Nutrition", on_delete=models.CASCADE)
+    allergens = models.ManyToManyField("Allergen")
+
+    class Meta:
+        db_table = "drinks"
+
+class Description(models.Model):
+    content = models.TextField()
+    drink   = models.ForeignKey("Drink", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "descriptions"
+
+class Image(models.Model):
+    url   = models.URLField(max_length=400)
+    drink = models.ForeignKey("Drink", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "images"
