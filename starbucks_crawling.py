65d89df3c52012efacd981c25962f424885ce19d
이충희 <choonghee.dev@gmail.com>
2020-08-08 10:58:25 +0900

    ADD: 음료 이름, 설명, 사이즈 크롤링
    [+] 음료의 한글 이름과 영어 이름 추출
    [+] 음료의 설명1, 설명2를 추출
    [+] 음료의 사이즈 정보 추출

--- starbucks_crawling.py
+++ starbucks_crawling.py
@@ -3,9 +3,7 @@ import re
 import time
 from pprint import pprint
 
-# from bs4 import BeautifulSoup
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
 
 TARGET_URL_LIST   = "https://www.starbucks.co.kr/menu/drink_list.do"
 TARGET_URL_DETAIL = "https://www.starbucks.co.kr/menu/drink_view.do?product_cd="
@@ -13,7 +11,6 @@ TARGET_URL_DETAIL = "https://www.starbucks.co.kr/menu/drink_view.do?product_cd="
 BS_PARSER = "html.parser"
 
 IMPLICIT_WAIT = 5
-
 
 # 스타벅스 음료 리스트 페이지 접속
 driver = webdriver.Chrome()
@@ -23,5 +20,24 @@ driver.implicitly_wait(IMPLICIT_WAIT)
 # 음료 제품 번호 가져오기
 go_drink_views = driver.find_elements_by_class_name('goDrinkView')
 product_numbers = [a.get_attribute('prod') for a in go_drink_views]
+
+for product_number in product_numbers:
+    driver.get(TARGET_URL_DETAIL + product_number)
+
+    # 한글 이름, 영어 이름
+    name_kr, name_en = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div[1]/h4').text.split('\n')
+    
+    # TODO: description1 == description2
+    description1_text = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[1]/div[2]/div[1]/p').text.split('\n')
+    description2_text = driver.find_element_by_xpath('//*[@id="container"]/div[2]/div[2]').text.split('\n')
+    description1      = ' '.join(description1_text)
+    description2      = ' '.join(description2_text)
+
+    # 사이즈 정보
+    size_text = driver.find_element_by_xpath('//*[@id="product_info01"]').text
+    size = re.sub('[^\w]|ml|fl|oz', ' ', size_text).split()
+    # pprint(size) # TODO: 현재는 리스트!
+    
+
 
 driver.quit()
